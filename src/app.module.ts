d63bd880586ff1d32a088d98f9c735c08cea91e2
Isaac-Lee <isaac.lee.3412@gmail.com>
2022-10-03 22:34:00 +0900

    add entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,9 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { ManagerModule } from './manager/manager.module';
+import { StoreModule } from './store/store.module';
+import { MenuModule } from './menu/menu.module';
+import { OrderlistModule } from './orderlist/orderlist.module';
 
 @Module({
   imports: [
@@ -17,8 +20,12 @@ import { ManagerModule } from './manager/manager.module';
       database: 'test',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
+      autoLoadEntities: true,
     }),
     ManagerModule,
+    StoreModule,
+    MenuModule,
+    OrderlistModule,
   ],
   controllers: [AppController],
   providers: [AppService],
