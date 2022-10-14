379a10e98636a290472f184b38786e1f55ae4a4a
haeramkeem <ewqdsacxz2345@gmail.com>
2022-10-14 23:26:17 +0900

    fix: environment variables for DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,11 +13,11 @@ import { OrderModule } from './order/order.module';
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '3412',
-      database: 'test',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT) || 3306,
+      username: process.env.DB_USERNAME || 'root',
+      password: process.env.DB_PASSWORD || '3412',
+      database: process.env.DB_NAME || 'test',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true, // 개발시에만 사용
       autoLoadEntities: true,
